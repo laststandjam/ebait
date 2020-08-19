25f7e0cfffe35b49ac3f1e9014128127bdc3db11
ihavehands <zaneshook@gmail.com>
2020-08-18 21:28:39 -0700

    fish hits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FishComponent } from './components/fish/fish.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FishComponent
   ],
   imports: [
     BrowserModule,
