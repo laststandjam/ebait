7ea74418585d5fe13b957fc1faa97d496ccb00d0
ihavehands <zaneshook@gmail.com>
2020-08-19 06:14:48 -0700

    routes and database

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-const express = require('express')
-const path =require('path')
-const bodyParser= require('body-parser')
-const cors = require('cors')
-const passport = require('passport')
-const mongoose = require('mongoose')
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const config = require('./config/database')
+console.log(config.database)
+mongoose.connect(config.database,{ useNewUrlParser: true } )
+mongoose.connection.on('connected', ()=>(
+    console.log('connected to database')
+))
 
-const app = express()
+const app = express();
 
-const port = 3000
+const users = require('./routes/users')
+const port = 3000;
 
-app.listen(port, ()=>{
-    console.log('server started on port '+port)
-})
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'client/src')))
+app.use(bodyParser.json())
+
+app.use('/users', users)
+
+app.get("/", (req, res) => res.send("Invalid Endpoint"));
+
+app.listen(port, () => {
+  console.log("server started on port " + port);
+});
---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const express = require('express'
+)
+const router = express.Router()
+
+router.get('/register', (req,res, next)=>{
+    res.send('REGISTER')
+})
+router.get('/authenticate', (req,res, next)=>{
+    res.send('AUTHENTICATE')
+})
+router.get('/profile', (req,res, next)=>{
+    res.send('PROFILE')
+})
+router.get('/VALIDATE', (req,res, next)=>{
+    res.send('VALIDATE')
+})
+
+
+module.exports = router
